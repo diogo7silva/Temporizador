b77f4618284301c3d41b763cfccbdc3fc2c60ffe
Diogo Silva <49910634+diogo7silva@users.noreply.github.com>
2019-11-04 17:53:42 +0000

    Add files via upload

---
+++ Temporizador/Temporizador-master/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Temporizador
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Start = new System.Windows.Forms.Button();
+            this.Stop = new System.Windows.Forms.Button();
+            this.Reset = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.minutos = new System.Windows.Forms.Button();
+            this.segundos = new System.Windows.Forms.Button();
+            this.horas = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Start
+            // 
+            this.Start.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(150)))), ((int)(((byte)(160)))), ((int)(((byte)(110)))));
+            this.Start.Font = new System.Drawing.Font("OCR A Extended", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Start.Location = new System.Drawing.Point(120, 336);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(140, 59);
+            this.Start.TabIndex = 0;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = false;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // Stop
+            // 
+            this.Stop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(120)))), ((int)(((byte)(100)))));
+            this.Stop.Font = new System.Drawing.Font("OCR A Extended", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Stop.Location = new System.Drawing.Point(548, 336);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(140, 59);
+            this.Stop.TabIndex = 1;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = false;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // Reset
+            // 
+            this.Reset.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(210)))), ((int)(((byte)(210)))));
+            this.Reset.Font = new System.Drawing.Font("OCR A Extended", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Reset.Location = new System.Drawing.Point(334, 336);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(140, 59);
+            this.Reset.TabIndex = 2;
+            this.Reset.Text = "Reset";
+            this.Reset.UseVisualStyleBackColor = false;
+            this.Reset.Click += new System.EventHandler(this.Reset_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(150)))), ((int)(((byte)(160)))), ((int)(((byte)(110)))));
+            this.label1.Font = new System.Drawing.Font("OCR A Extended", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(120, 138);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(568, 167);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "00:00:00";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // minutos
+            // 
+            this.minutos.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(210)))), ((int)(((byte)(210)))));
+            this.minutos.Font = new System.Drawing.Font("OCR A Extended", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minutos.Location = new System.Drawing.Point(334, 49);
+            this.minutos.Name = "minutos";
+            this.minutos.Size = new System.Drawing.Size(140, 59);
+            this.minutos.TabIndex = 6;
+            this.minutos.Text = "Minutos";
+            this.minutos.UseVisualStyleBackColor = false;
+            this.minutos.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Acertar_Relogio);
+            // 
+            // segundos
+            // 
+            this.segundos.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(210)))), ((int)(((byte)(210)))));
+            this.segundos.Font = new System.Drawing.Font("OCR A Extended", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.segundos.Location = new System.Drawing.Point(548, 49);
+            this.segundos.Name = "segundos";
+            this.segundos.Size = new System.Drawing.Size(140, 59);
+            this.segundos.TabIndex = 5;
+            this.segundos.Text = "Segundos";
+            this.segundos.UseVisualStyleBackColor = false;
+            this.segundos.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Acertar_Relogio);
+            // 
+            // horas
+            // 
+            this.horas.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(210)))), ((int)(((byte)(210)))));
+            this.horas.Font = new System.Drawing.Font("OCR A Extended", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.horas.Location = new System.Drawing.Point(120, 49);
+            this.horas.Name = "horas";
+            this.horas.Size = new System.Drawing.Size(140, 59);
+            this.horas.TabIndex = 4;
+            this.horas.Text = "Horas";
+            this.horas.UseVisualStyleBackColor = false;
+            this.horas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Acertar_Relogio);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.minutos);
+            this.Controls.Add(this.segundos);
+            this.Controls.Add(this.horas);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Relógio Digital";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.Button Reset;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button minutos;
+        private System.Windows.Forms.Button segundos;
+        private System.Windows.Forms.Button horas;
+    }
+}
+
---
+++ Temporizador/Temporizador-master/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Text;
+
+// Bibliotecas para adicionar fontes :
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+
+namespace Temporizador
+{
+    public partial class Form1 : Form
+    {
+        private int contador;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            LoadFont();
+            label1.Font = new Font(private_fonts.Families[0], 72);
+            label1.UseCompatibleTextRendering = true;
+            LCD(Start, 22); LCD(Stop, 22); LCD(Reset, 22);
+            LCD(horas, 16); LCD(minutos, 16); LCD(segundos, 16);
+        }
+
+        private void LCD(object sender, int tamanho)
+        {
+            ((Button)sender).Font = new Font(private_fonts.Families[0], tamanho);
+            ((Button)sender).UseCompatibleTextRendering = true;
+        }
+
+        // Função para adicionar fonte LCD :
+        private readonly PrivateFontCollection private_fonts = new PrivateFontCollection();
+        private void LoadFont()
+        {
+            string resource = "Temporizador.digital-7-mono.ttf";
+            Stream fontStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resource);
+            System.IntPtr data = Marshal.AllocCoTaskMem((int)fontStream.Length);
+            byte[] fontdata = new byte[fontStream.Length];
+            fontStream.Read(fontdata, 0, (int)fontStream.Length);
+            Marshal.Copy(fontdata, 0, data, (int)fontStream.Length);
+            private_fonts.AddMemoryFont(data, (int)fontStream.Length);
+            fontStream.Close();
+            Marshal.FreeCoTaskMem(data);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            contador = 0;
+            timer1.Interval = 1000;
+        }
+
+        private void mostrar()
+        {
+            Console.Beep();
+            int dia = 24 * (int)Math.Pow(60, 2);
+            if (contador < 0) contador += dia;
+            if (contador >= dia) contador -= dia;
+            label1.Text = TimeSpan.FromSeconds(contador).ToString();
+        }
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            contador = 0;
+            mostrar();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            contador++;
+            mostrar();
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void Acertar_Relogio(object sender, MouseEventArgs e)
+        {
+            int elev = 0;
+            switch (((Button)sender).Name)
+            {
+                case "segundos": elev = 0; break;
+                case "minutos": elev = 1; break;
+                case "horas": elev = 2; break;
+            }
+            int valor = (int)Math.Pow(60, elev);
+            contador += (e.Button == MouseButtons.Left) ? valor : -valor;
+            mostrar();
+        }
+
+    }
+}
